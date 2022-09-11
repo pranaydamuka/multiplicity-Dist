a83687a944bc998c41b4e0fc66b0a7c6a23c986c
pranaydamuka <34735684+pranaydamuka@users.noreply.github.com>
2022-09-11 11:27:50 +0530

    Add files via upload

--- kno.c
+++ kno.c
@@ -6,21 +6,26 @@ void kno() {
    TGraph *gr2 = new TGraph();
    TGraph *gr3 = new TGraph();
    TGraph *gr4 = new TGraph();
-   TCanvas *c1 = new TCanvas();
+   TCanvas *c1 = new TCanvas("c1","multipads", 600, 500);
    //gPad->SetGrid();
    gPad->SetLogy(1);
    //reading into the 1st file
+   
+
    fstream file1;
    file1.open("7000exp24.txt", ios::in);
    int n = 0;
    while(1)
         {
         
-          double a, b, c, d;
+           double a, b, c, d;
           file1 >> a >> b >> c >> d;
+          //double a, b, c, d, e, f; //for 2.36 only
+          //file1 >> a >> b >> c >> d >> e >> f; //for 2.36 only
           n = gr1->GetN();
           gr1->SetPoint(n, a, b);
-          gr1->SetPointError(n, 0, 0, c, d);
+          gr1->SetPointError(n, 0, 0, d, c);
+          //gr1->SetPointError(n, c, d, e, f); // for 2.36 only
 
           if(file1.eof()) break;
         
@@ -28,10 +33,10 @@ void kno() {
         file1.close();
         gr1->SetMarkerStyle(kOpenCircle);
         gr1->SetMarkerSize(1.3);
-        gr1->SetMarkerColor(kRed);
-        gr1->SetLineColor(kRed);
+        gr1->SetMarkerColor(kBlack);
+        gr1->SetLineColor(kBlack);
    //reading into the 2nd file
-   
+  
    fstream file2;
    file2.open("cap7000the24.txt", ios::in);
    while(1)
@@ -46,14 +51,14 @@ void kno() {
         file2.close();
    
         gr2->SetMarkerSize(1);
-        gr2->SetMarkerColor(kOrange);
-        gr2->SetLineColor(kOrange);
-            
+        gr2->SetMarkerColor(46);
+        gr2->SetLineColor(46);
+        gr2->SetLineWidth(2);     
    // reading into the 3rd file
    
   
    fstream file3;
-   file3.open("o7000_24_4C_psiz.txt", ios::in);
+   file3.open("7000_24_4C_psiz.txt", ios::in);
    while(1)
         {
         
@@ -68,10 +73,10 @@ void kno() {
         gr3->SetMarkerSize(1);
         gr3->SetMarkerColor(kBlue);
         gr3->SetLineColor(kBlue);
-        gr3->SetLineStyle(9);
-    
+        gr3->SetLineStyle(8);
+        gr3->SetLineWidth(3);
    fstream file4;
-   file4.open("o7000_24_monash_psiz.txt", ios::in);
+   file4.open("7000_24_monash_psiz.txt", ios::in);
    while(1)
         {
         
@@ -84,11 +89,11 @@ void kno() {
         file4.close();
    
         gr4->SetMarkerSize(1);
-        gr4->SetMarkerColor(kBlack);
-        gr4->SetMarkerStyle(kDot);
-        gr4->SetLineColor(kBlack);
-        gr4->SetLineStyle(kDotted);
-        gr4->SetLineWidth(2);   
+        gr4->SetMarkerColor(kRed);
+        gr4->SetMarkerStyle(3);
+        gr4->SetLineColor(kRed);
+        gr4->SetLineStyle(3);
+        gr4->SetLineWidth(3);   
     
    //mg->Add(gr1, "AC");
    mg->Add(gr1, "APE");
@@ -100,24 +105,26 @@ void kno() {
     mg->GetYaxis()->SetTitle("#psi(z)");
     mg->GetYaxis()->CenterTitle(true);
     mg->GetXaxis()->CenterTitle(true);
-    mg->GetYaxis()->SetLabelSize(0.04);
-    mg->GetXaxis()->SetLabelSize(0.04);
-    mg->GetYaxis()->SetTitleSize(0.045);
-    mg->GetXaxis()->SetTitleSize(0.045);
-    mg->GetYaxis()->SetTitleOffset(0.9);
+    mg->GetYaxis()->SetLabelSize(0.03);
+    mg->GetXaxis()->SetLabelSize(0.033);
+    mg->GetYaxis()->SetTitleSize(0.05);
+    mg->GetXaxis()->SetTitleSize(0.05);
+    mg->GetYaxis()->SetTitleOffset(1.025);
     mg->GetXaxis()->SetTitleOffset(0.9);
-   mg->GetYaxis()->SetRangeUser(2.45*pow(10, -4), 5);
-   mg->GetXaxis()->SetRangeUser(0.0, 7);
-   TLegend *leg = new TLegend(0.70,0.72,0.88,0.86);
+   mg->GetYaxis()->SetRangeUser(2.45*pow(10, -4), 8.0);
+   mg->GetXaxis()->SetRangeUser(0.0, 6.5);
+   //mg->GetYaxis()->SetRangeUser(3.45*pow(10, -2), 5); // for 0 to 2.5 range
+   //mg->GetXaxis()->SetRangeUser(0.0, 2.5245);
+   TLegend *leg = new TLegend(0.6,0.7,0.89,0.88);
    leg->AddEntry(gr1, "CMS Data", "kOpenCircle");
-   leg->AddEntry(gr2, "Theoretical Prediction", "l");
+   leg->AddEntry(gr2, "Theory", "l");
    leg->AddEntry(gr3, "Pythia 8 4C ", "l");
    leg->AddEntry(gr4, "Pythia 8 Monash ", "l");
    leg->SetBorderSize(0);
    leg->Draw();
-   TLegend *leg1 = new TLegend(0.3, 0.835, 0.6, 0.88);
+   TLegend *leg1 = new TLegend(0.18, 0.75, 0.55, 0.892);
    leg1->AddEntry((TObject*)0, "#sqrt{s} = 7 TeV, |#eta| < 2.4", "c");
    leg1->SetBorderSize(0);
    leg1->Draw();   
-
+   //c1->Print("p7000_24_psiz_0to2.5.pdf");
 }
